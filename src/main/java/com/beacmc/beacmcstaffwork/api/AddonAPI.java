afc066758eae02243bf91a964f3cd391b62fbf6f
beacmc <kornieiko09@gmail.com>
2023-09-26 09:20:02 +0300

    new code

--- src/main/java/com/beacmc/beacmcstaffwork/api/AddonAPI.java
+++ src/main/java/com/beacmc/beacmcstaffwork/api/AddonAPI.java
@@ -1,2 +1,27 @@
-package com.beacmc.beacmcstaffwork.api;public class AddonAPI {
+package com.beacmc.beacmcstaffwork.api;
+
+import com.beacmc.beacmcstaffwork.api.manager.CustomAddonHook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddonAPI {
+
+    private static Map<String, CustomAddonHook> addons;
+
+    public static boolean registerAddonHook(final CustomAddonHook addon) {
+        if(addon.getCommand() == null || addon == null || AddonAPI.addons.containsKey(addon.getCommand())) {
+            return false;
+        }
+        AddonAPI.addons.put(addon.getCommand(), addon);
+        return true;
+    }
+
+    public static Map<String, CustomAddonHook> getAddons() {
+        return AddonAPI.addons;
+    }
+
+    static {
+        AddonAPI.addons = new HashMap<String, CustomAddonHook>();
+    }
 }
--- src/main/java/com/beacmc/beacmcstaffwork/commands/StaffAdminCommand.java
+++ src/main/java/com/beacmc/beacmcstaffwork/commands/StaffAdminCommand.java
@@ -1,2 +1,32 @@
-package com.beacmc.beacmcstaffwork.commands;public class StaffAdminCommand {
+package com.beacmc.beacmcstaffwork.commands;
+
+import com.beacmc.beacmcstaffwork.BeacmcStaffWork;
+import com.beacmc.beacmcstaffwork.manager.Command;
+import com.beacmc.beacmcstaffwork.manager.User;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StaffAdminCommand extends Command {
+    public StaffAdminCommand() {
+        super("staffworkadmin");
+    }
+
+    @Override
+    public void execute(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        User user = new User((Player) sender, BeacmcStaffWork.getInstance());
+
+        if(user.hasPermission("beacmcstaffwork.admin")) {
+            user.sendMessage("settings.messages.no-permission");
+            return;
+        }
+
+        if (args.length < 1 || args.length > 1) {
+            return;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            BeacmcStaffWork.getInstance().reloadConfig();
+            user.sendMessage("settings.messages.reload");
+        }
+    }
 }
